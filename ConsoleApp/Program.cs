2198de3224bc3b0131ea5b97f301352c81a589f8
Oleksandr Hryshchuk <Oleksandr.Hryshchuk@mask-me.net>
2021-02-22 15:16:19 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WpfLibrary.TestWindow testWindow = null;
+            Thread thread = new Thread(()=> {
+                testWindow = new WpfLibrary.TestWindow();
+                testWindow.TryInitWebView2();
+                testWindow.ShowDialog();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
